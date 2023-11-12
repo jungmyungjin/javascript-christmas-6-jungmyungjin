13313e4ac0f388978392ebf273ad6a81bcac0680
jungmyungjin <myung2667@gmail.com>
2023-11-13 01:15:51 +0900

    fix(inputView, OutputView) : 추가 공지 수정파일 적용

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,10 @@
-export default OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
-    }
+OutputView.js;
+const OutputView = {
+  printMenu() {
+    Console.print("<주문 메뉴>");
     // ...
-}
+  },
+  // ...
+};
+
+export default OutputView;
