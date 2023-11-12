a6bbae9f4fba743448c6dc3bdb1b4bcb7d2986ad
jungmyungjin <myung2667@gmail.com>
2023-11-13 05:05:22 +0900

    fix(inputView) : 입력 문구 뒤에 줄바꿈 문자 추가

--- src/Interface/InputView.js
+++ src/Interface/InputView.js
@@ -3,13 +3,13 @@ import { Console } from "@woowacourse/mission-utils";
 const InputView = {
   async readDate() {
     const input = await Console.readLineAsync(
-      "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"
+      "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n"
     );
     return input;
   },
   async readMenu() {
     const input = await Console.readLineAsync(
-      "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"
+      "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n"
     );
     return input;
   },
