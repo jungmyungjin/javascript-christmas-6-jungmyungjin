34b745acc0cc0fadef0e5a67fc1a9059db328bfe
jungmyungjin <myung2667@gmail.com>
2023-11-13 19:18:24 +0900

    feat(App): App 클래스 동작 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-class App {
-  async run() {}
-}
+import PaymentStatement from "./Domain/PaymentStatement.js";
+import { Console } from "@woowacourse/mission-utils";
+import Input from "./Interface/Input.js";
 
+class App {
+  async run() {
+    try {
+      const input = new Input();
+      await input.start();
+      const [date, order] = [input.getInputDate(), input.getInputOrder()];
+      const paymentStatementInstance = new PaymentStatement(order, date);
+      paymentStatementInstance.printPaymentStatement();
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
+}
 export default App;
