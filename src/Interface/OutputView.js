229a72dd3ffe3b93022e3602e20d7777e11657c4
jungmyungjin <myung2667@gmail.com>
2023-11-13 14:32:40 +0900

    fix(OutputView) : 출력 시 필요한 매개변수 추가

--- src/Interface/OutputView.js
+++ src/Interface/OutputView.js
@@ -4,32 +4,59 @@ const OutputView = {
     Console.print("<주문 메뉴>");
     // ...
   },
-  print() {
+  printPreviewPaymentStatement() {
     Console.print("12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+    Console.print("\n");
   },
   printGreeting() {
     Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
   },
-  printTotalOrderAmount() {
+  printTotalOrderAmount(totalOrderAmount) {
     Console.print("<할인 전 총주문 금액>");
+    Console.print(`${totalOrderAmount.toLocaleString("ko-KR")}원`);
+    Console.print("\n");
   },
-  printGiftMenu(gift) {
+  printGiftMenu(giveaway) {
     Console.print("<증정 메뉴>");
-    if (!gift) {
+    if (!giveaway) {
       Console.print("없음");
+      return;
     }
+    for (const gift in giveaway) {
+      Console.print(`${gift} ${giveaway[gift]}개`);
+    }
+    Console.print("\n");
   },
-  printTotalDiscount(Discount) {
+  printBenefitDetails(benefits) {
+    Console.print("<혜택 내역>");
+    if (!benefits) {
+      Console.print("없음");
+      return;
+    }
+    for (const title in benefits) {
+      if (!benefits[title]) continue;
+      Console.print(`${title}: -${benefits[title].toLocaleString("ko-KR")}원`);
+    }
+    Console.print("\n");
+  },
+  printTotalDiscountAmount(totalDiscount) {
     Console.print("<총혜택 금액>");
-    if (!Discount) {
+    if (!totalDiscount) {
       Console.print("없음");
+      return;
     }
+    Console.print(`${totalDiscount.toLocaleString("ko-KR")}원`);
+    Console.print("\n");
   },
-  printFinalPayment() {
+  printFinalPayment(finalPayment) {
     Console.print("<할인 후 예상 결제 금액>");
+    Console.print(`${finalPayment.toLocaleString("ko-KR")}원`);
+    Console.print("\n");
   },
-  printEventBadge() {
+  printEventBadge(badge) {
     Console.print("<12월 이벤트 배지>");
+    Console.print(badge);
+    Console.print("\n");
   },
 };
 
